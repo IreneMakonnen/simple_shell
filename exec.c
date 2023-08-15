bca75a60f6e6d385bd626957c7612c274c693488
belZ1232 <Belzsena@gmail.com>
2023-08-15 09:33:14 -0700

    Task 1

--- exec.c
+++ exec.c
@@ -1,70 +1,57 @@
 #include "shell.h"
-/* display_prompt - func dat print prompt text
- * display the prompt to user
- */
 
+/**
+* display_prompt - Func dat displays prompt to user.
+*/
 void display_prompt(void)
 {
 printf("Simple_Shell$ ");
 fflush(stdout);
 }
 /**
-* execute_command - Function that execute command
+* execute_command - Func dat executes a command.
 *
-* @char: Command to execute
-* exit te block
-* @ strcspn: Remove a trailing newline char
-* @ comma: Pointer to a character
-* Return: if execution failed bring 1
-* or 0 if successfull
+* @comma: Its a command to execute.
+* Return: 1 if execution failed
+* or 0 if successful.
 */
-
 int execute_command(char *comma)
 {
-
 if (comma == NULL)
 {
 printf("\n");
 exit(0);
 }
-
 comma[strcspn(comma, "\n")] = '\0';
-
 int res = system(comma);
-
 if (res == -1)
 {
-printf("Execution of comma failed.\n");
+printf("Execution of command failed.\n");
 return (1);
 }
-
 return (0);
 }
 /**
-* main - Entry point
-* @Handles end of file and exit the block
-* Return: 0 (successful)
+* main - Entry point of the program.
+*
+* Handles end of file and exits the block.
+* Return: 0 (successful).
 */
 int main(void)
 {
 char comma[MAX_COMMAND_LENGTH];
-
 while (1)
 {
 display_prompt();
-
 if (fgets(comma, sizeof(comma), stdin) == NULL)
 {
 printf("\n");
 break;
 }
-
 if (execute_command(comma) != 0)
 {
 printf("Command not found: %s\n", comma);
 }
 }
-
 return (0);
 }
-
