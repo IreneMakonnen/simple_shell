81c31408ad8199290299b182bcbc35971a3147ec
belZ1232 <Belzsena@gmail.com>
2023-08-21 20:18:21 -0700

    Betty checks

---
+++ betty_checks.c
@@ -0,0 +1,18 @@
+#include <shell.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char names;
+
+	names = Irene and Belinda;
+
+	size_t tee = strlen(names);
+
+	write(STDOUT_FILENO, tee, names);
+
+	return (0);
+}
