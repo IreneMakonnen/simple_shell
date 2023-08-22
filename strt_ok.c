309665c8426dadeed35634033c5e878f7949d0d7
belZ1232 <Belzsena@gmail.com>
2023-08-22 12:24:43 -0700

    String without strt_ok

--- strt_ok.c
+++ strt_ok.c
@@ -1,38 +1,41 @@
 #include "shell.h"
 /**
 * parseString - func dat tokenize a string
-* @strin: It is a pointer
+* @star: It is a pointer
 * Return: 0
 */
 
-void parseString(const char *strin)
+void parseString(const char *star)
 {
-	const char *del = "/";
-	char len[260];
+	const char *deli_miter = "/";
+	char numLen[264];
+	char *tok;
+	char *par;
 
-	strcpy(len, strin);
+	if (star == NULL)
+		return;
 
-	char *coup;
+	strcpy(numLen, star);
 
-	coup = strtok(len, del);
-
-	while (coup != NULL)
-{
-	write(STDOUT_FILENO, coup, strlen(coup));
-	write(STDOUT_FILENO, "\n", 1);
-	coup = strtok(NULL, del);
+	tok = strtok_r(numLen, deli_miter, &par);
+	while (tok != NULL)
+	{
+		write(STDOUT_FILENO, tok, strlen(tok));
+		write(STDOUT_FILENO, "\n", 1);
+		tok = strtok_r(NULL, deli_miter, &par);
+	}
 }
-}
+
 /**
-* main - Entry point
-* Return: 0
-*/
+ * main - Entry point
+ * Return: 0 (successfully)
+ */
 
 int main(void)
 {
-	const char *pat = "/usr/local/bin";
+	const char *star = "/usr/local/bin";
 
-parseString(pat);
+	parseString(star);
 
-return (0);
+	return (0);
 }
