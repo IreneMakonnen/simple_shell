6853f7c03de2737fb9d129b1d6203a185b76f919
belZ1232 <Belzsena@gmail.com>
2023-08-23 13:25:16 -0700

    Betty checks

--- betty_checks.c
+++ betty_checks.c
@@ -3,17 +3,25 @@
 void print_names(const char *our_names, size_t num_names);
 /**
  * main - Entry point
+ * @argc: number of command line passed to code
+ * @argv: an array of string containing arguments
  * Return: 0 successfully
  */
-int main(void)
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
 
-	const char *our_names[] = {
+	const char *our_names[];
+	size_t *num_names;
+       our_names = {
 		"Irene",
 		"Belinda"
 	};
-	size_t *num_names = sizeof(our_names) / sizeof(our_names[0]);
 
-	print_names(our_names[0], num_names);
+	num_names = sizeof(our_names) / sizeof(our_names[0]);
+
+	print_names(our_names, num_names);
 
 	return (0);
 /**
@@ -23,13 +31,15 @@ int main(void)
  * @num_names: size
  */
 
-void print_names(const char *our_names, size_t num_names)
+void print_names(const char **our_names, size_t num_names)
 
 	for (size_t a = 0; a < num_names; a++)
 	{
+		const char *our_names = our_names[a];
 		size_t len_gth = strlen(our_names);
 
 		write(STDOUT_FILENO, our_names, len_gth);
 		write(STDOUT_FILENO, "\n", 1);
 		our_names += len_gth + 1;/* Move the pointer to next name */
+	}
 }
