278d08b2aa006a68fcb139ed14bd3c9a59813958
belZ1232 <Belzsena@gmail.com>
2023-08-23 10:14:46 -0700

    Betty checks

--- betty_checks.c
+++ betty_checks.c
@@ -32,4 +32,4 @@ void print_names(const char *our_names, size_t num_names)
 		write(STDOUT_FILENO, our_names, len_gth);
 		write(STDOUT_FILENO, "\n", 1);
 		our_names += len_gth + 1;/* Move the pointer to next name */
-	}
+}
