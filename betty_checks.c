3e570892913b006a8607fa19935bc79ff5bac50c
belZ1232 <Belzsena@gmail.com>
2023-08-23 09:51:47 -0700

    Betty checks

--- betty_checks.c
+++ betty_checks.c
@@ -1,15 +1,32 @@
 #include "shell.h"
+
+void print_names(const char *our_names, size_t num_names);
 /**
  * main - Entry point
- * Return: 0
+ * Return: 0 successfully
  */
-
 int main(void)
-{
-	const char *names = "Irene", "Belinda";
-	size_t numLen = strlen(names);
 
-	write(STDOUT_FILENO, numLen, names);
+	const char *our_names[] = {"Irene\nBelinda"};
+	size_t num_names = sizeof(our_names) / sizeof(our_names[0]);
+
+	print_names(our_names[0], num_names);
 
 	return (0);
-}
+/**
+ * print_names - modified to iterate
+ * over our names correctly.
+ * @our_names: pointer
+ * @num_names: size
+ */
+
+void print_names(const char *our_names, size_t num_names)
+
+	for (size_t a = 0; a < num_names; a++)
+	{
+		size_t len_gth = strlen(our_names);
+
+		write(STDOUT_FILENO, our_names, len_gth);
+		write(STDOUT_FILENO, "\n", 1);
+		our_names += len_gth + 1;/* Move the pointer to next name */
+	}
